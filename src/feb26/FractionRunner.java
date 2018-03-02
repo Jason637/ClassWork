58c65287bb12dd95256e1b1584b11279fb00ed77
Jason637 <Jason.cheng64@yahoo.com>
2018-03-02 09:17:39 -0600

    created to string fixed some missing things need to find out what is causing the issue in the 4th line printed

--- src/feb26/FractionRunner.java
+++ src/feb26/FractionRunner.java
@@ -3,11 +3,14 @@ import java.util.Arrays;
 import java.util.List;
 public class FractionRunner
 {
-   public static void main(String[] args) {
+   public static void main(String[] args)
+   {
        Fraction f1 = new Fraction(1, 4);
        Fraction f2 = new Fraction(1, 3);
        Fraction f3 = f1;
        Fraction[] fractions= new Fraction[2];
+       fractions[0]=f1;
+       fractions[1]=f2;
        System.out.println(f1.compareTo(f2));
        String s1 = "Frog";
        String s2 = "Frog";
